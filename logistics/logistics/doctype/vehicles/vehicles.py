671af453b1e2ae7909fee2eaaadb087d9942ded0
sivajyothis7 <sivajyothis9446@gmail.com>
2023-09-27 10:50:56 +0530

    test

--- logistics/logistics/doctype/vehicles/vehicles.py
+++ logistics/logistics/doctype/vehicles/vehicles.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2023, siva and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Vehicles(Document):
-	pass
+	def validate(self):
+		frappe.db.set_value("Drivers",{'name':self.current_driver},'current_vehicle',self.current_driver)
