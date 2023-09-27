8b3b11d441d8d7458e059a49be7dfa79fd3b3f48
sivajyothis7 <sivajyothis9446@gmail.com>
2023-09-27 20:09:07 +0530

    vehicle_button

--- logistics/logistics/doctype/job_details/job_details.js
+++ logistics/logistics/doctype/job_details/job_details.js
@@ -14,27 +14,32 @@ frappe.ui.form.on('Job Details', {
                         var vehicle_options = r.message.map(function(vehicle) {
                             return {
                                 label: vehicle.name,
-                                value: vehicle.name1,
+                                value: vehicle.name,
+                                name1: vehicle.name1,
                             };
                         });
 
                         // Show a select field with vehicle options
                         frappe.prompt({
                             fieldname: 'selected_vehicle',
-                            label: __('Select Vehicle'),
+                            label: __('Please Select Vehicle'),
                             fieldtype: 'Select',
                             options: vehicle_options,
                         }, function(values) {
                             // Handle the selected vehicle here
-							var selected_vehicle = values.selected_vehicle;
-							var row = frm.add_child('vehicle', {});
-                            row.vehicle_name = selected_vehicle;
-							row.data=selected_vehicle.id
-                            frm.refresh_field('vehicle');
-
-
-                           
-                            // frm.set_value('selected_vehicle', selected_vehicle);
+                            var selected_vehicle = values.selected_vehicle;
+                            var selected_name1 = vehicle_options.find(option => option.value === selected_vehicle).name1;
+                            
+                            // Check if the selected vehicle already exists in the child table
+                            var existing_vehicles = frm.doc.vehicle || [];
+                            if (!existing_vehicles.some(vehicle => vehicle.data === selected_vehicle)) {
+                                var row = frm.add_child('vehicle', {});
+                                row.data = selected_vehicle;
+                                row.vehicle_name = selected_name1; // Set row.vehicle_name to the name1 field
+                                frm.refresh_field('vehicle');
+                            } else {
+                                frappe.msgprint(__('Selected vehicle already exists in Vehicles Data.'));
+                            }
                         }, __('Select Vehicle'));
                     }
                 }
