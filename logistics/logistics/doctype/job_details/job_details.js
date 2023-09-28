a604e777ce19a14022b244b692c3404fba4c0df0
sivajyothis7 <sivajyothis9446@gmail.com>
2023-09-28 09:39:12 +0530

    vehicle_logic

--- logistics/logistics/doctype/job_details/job_details.js
+++ logistics/logistics/doctype/job_details/job_details.js
@@ -1,8 +1,6 @@
 frappe.ui.form.on('Job Details', {
     refresh: function(frm) {
-        // Add a custom button to fetch and display vehicle list
         frm.add_custom_button(__('Select Vehicle'), function() {
-            // Fetch the list of vehicles from the "Vehicles" doctype
             frappe.call({
                 method: 'frappe.client.get_list',
                 args: {
@@ -19,23 +17,20 @@ frappe.ui.form.on('Job Details', {
                             };
                         });
 
-                        // Show a select field with vehicle options
                         frappe.prompt({
                             fieldname: 'selected_vehicle',
                             label: __('Please Select Vehicle'),
                             fieldtype: 'Select',
                             options: vehicle_options,
                         }, function(values) {
-                            // Handle the selected vehicle here
                             var selected_vehicle = values.selected_vehicle;
                             var selected_name1 = vehicle_options.find(option => option.value === selected_vehicle).name1;
                             
-                            // Check if the selected vehicle already exists in the child table
                             var existing_vehicles = frm.doc.vehicle || [];
                             if (!existing_vehicles.some(vehicle => vehicle.data === selected_vehicle)) {
                                 var row = frm.add_child('vehicle', {});
                                 row.data = selected_vehicle;
-                                row.vehicle_name = selected_name1; // Set row.vehicle_name to the name1 field
+                                row.vehicle_name = selected_name1; 
                                 frm.refresh_field('vehicle');
                             } else {
                                 frappe.msgprint(__('Selected vehicle already exists in Vehicles Data.'));
