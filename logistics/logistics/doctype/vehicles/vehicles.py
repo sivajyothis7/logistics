66633c44288e56707ad71627a115437af8b6fd45
sivajyothis7 <sivajyothis9446@gmail.com>
2023-09-28 10:16:14 +0530

    status

--- logistics/logistics/doctype/vehicles/vehicles.py
+++ logistics/logistics/doctype/vehicles/vehicles.py
@@ -5,6 +5,12 @@ import frappe
 from frappe.model.document import Document
 
 class Vehicles(Document):
-	def validate(self):
-		frappe.db.set_value("Drivers",{'name':self.current_driver},'status','Vehicle Assigned')	
-		frappe.db.set_value("Drivers",{'name':self.current_driver},'current_vehicle',self.name)
+    def validate(self):
+        if self.current_driver:
+            frappe.db.set_value("Drivers", {'name': self.current_driver}, 'status', 'Vehicle Assigned')    
+            frappe.db.set_value("Drivers", {'name': self.current_driver}, 'current_vehicle', self.name)
+        else:
+            driver_name = frappe.db.get_value("Drivers", {"current_vehicle": self.name}, "name")
+            if driver_name:
+                frappe.db.set_value("Drivers", {'name': driver_name}, 'status', 'Vehicle Not Assigned')    
+                frappe.db.set_value("Drivers", {'name': driver_name}, 'current_vehicle', '')
