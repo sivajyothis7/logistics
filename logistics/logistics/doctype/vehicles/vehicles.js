5c4f32df19e5a0e506019c426ba29a7affc0aab0
sivajyothis7 <sivajyothis9446@gmail.com>
2023-10-04 12:34:21 +0530

    new_flow

--- logistics/logistics/doctype/vehicles/vehicles.js
+++ logistics/logistics/doctype/vehicles/vehicles.js
@@ -5,13 +5,13 @@ frappe.ui.form.on('Vehicles', {
 	// refresh: function(frm) {
 
 	// }
-	setup: function(frm) {
-		frm.set_query("current_driver", function() {
-			return {
-				filters: [
-					["Drivers","status","in",["Vehicle Not Assigned"]]
-				]
-			};
-		});
-	}
+	// setup: function(frm) {
+	// 	frm.set_query("current_driver", function() {
+	// 		return {
+	// 			filters: [
+	// 				["Drivers","status","in",["Vehicle Not Assigned"]]
+	// 			]
+	// 		};
+	// 	});
+	// }
 });
--- logistics/logistics/doctype/vehicles/vehicles.py
+++ logistics/logistics/doctype/vehicles/vehicles.py
@@ -5,12 +5,13 @@ import frappe
 from frappe.model.document import Document
 
 class Vehicles(Document):
-    def validate(self):
-        if self.current_driver:
-            frappe.db.set_value("Drivers", {'name': self.current_driver}, 'status', 'Vehicle Assigned')    
-            frappe.db.set_value("Drivers", {'name': self.current_driver}, 'current_vehicle', self.name)
-        else:
-            driver_name = frappe.db.get_value("Drivers", {"current_vehicle": self.name}, "name")
-            if driver_name:
-                frappe.db.set_value("Drivers", {'name': driver_name}, 'status', 'Vehicle Not Assigned')    
-                frappe.db.set_value("Drivers", {'name': driver_name}, 'current_vehicle', '')
+    pass
+    # def validate(self):
+    #     if self.current_driver:
+    #         frappe.db.set_value("Drivers", {'name': self.current_driver}, 'status', 'Vehicle Assigned')    
+    #         frappe.db.set_value("Drivers", {'name': self.current_driver}, 'current_vehicle', self.name)
+    #     else:
+    #         driver_name = frappe.db.get_value("Drivers", {"current_vehicle": self.name}, "name")
+    #         if driver_name:
+    #             frappe.db.set_value("Drivers", {'name': driver_name}, 'status', 'Vehicle Not Assigned')    
+    #             frappe.db.set_value("Drivers", {'name': driver_name}, 'current_vehicle', '')
