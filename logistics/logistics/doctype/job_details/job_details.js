b5320d7401c64d02fb49c44ee7a3a787d3ec1f8c
Amalghoshv <amalghoshv@gmail.com>
2023-10-09 09:48:12 +0530

    minor changes

--- logistics/logistics/doctype/job_details/job_details.js
+++ logistics/logistics/doctype/job_details/job_details.js
@@ -8,23 +8,29 @@ frappe.ui.form.on('Job Details', {
 				setters: {
 					// schedule_date: null,
 					company:null,
-                    date:null
+                    date:null,
 					
-					// status: 'Pending'
+					
 				},
 				add_filters_group: 1,
 				date_field: "transaction_date",
+				allow_child_item_selection: 1,
+				child_fieldname: "driver",
 				get_query() {
 					return {
-						filters: { docstatus: ['!=', 1] }
+						filters: { docstatus: ['!=', 2] }
 					}
 				},
+				primary_action_label: 'Get Details',
 				action(selections) {
+					console.log(selections)
 					selections.forEach(selectedItem => {
-                        const driver = selectedItem.data.driver; 
-                        const vehicleName = selectedItem.data.vehicle; 
+						console.log(selectedItem)
+                        const driver = selectedItem.driver; 
+                        const vehicleName = selectedItem.vehicle; 
 
                         // Append a new row to the "vehicle" table
+						console.log(frm.doc.driver)
                         const newRow = frm.add_child('vehicle');
                         newRow.driver = driver;
                         newRow.vehicle_name = vehicleName;
