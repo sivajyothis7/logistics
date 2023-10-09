e1a58f5599b8a1377c3295060d8f0175564782b3
Amalghoshv <amalghoshv@gmail.com>
2023-10-09 18:15:32 +0530

    dialogue filter

--- logistics/logistics/doctype/job_details/job_details.js
+++ logistics/logistics/doctype/job_details/job_details.js
@@ -1,8 +1,9 @@
 frappe.ui.form.on('Job Details', {
     refresh: function(frm) {
+		let d;
         frm.add_custom_button(__('Select Daily Log'), function() {
             // MultiSelectDialog for individual child selection
-			new frappe.ui.form.MultiSelectDialog({
+			 d = new frappe.ui.form.MultiSelectDialog({
 				doctype: "Daily Log",
 				target: frm,
 				setters: {
@@ -11,39 +12,57 @@ frappe.ui.form.on('Job Details', {
                     date:null,
 					
 					
+					
 				},
 				add_filters_group: 1,
 				date_field: "transaction_date",
-				allow_child_item_selection: 1,
-				child_fieldname: "driver",
+				columns: ["vehicle", "driver"],
 				get_query() {
 					return {
 						filters: { docstatus: ['!=', 2] }
 					}
 				},
 				primary_action_label: 'Get Details',
-				action(selections) {
-					console.log(selections)
+				action(selections, args) {
+					// Iterate through selected items
 					selections.forEach(selectedItem => {
-						console.log(selectedItem)
-                        const driver = selectedItem.driver; 
-                        const vehicleName = selectedItem.vehicle; 
-
-                        // Append a new row to the "vehicle" table
-						console.log(frm.doc.driver)
-                        const newRow = frm.add_child('vehicle');
-                        newRow.driver = driver;
-                        newRow.vehicle_name = vehicleName;
-                    });
-
-                    // Refresh the "vehicle" table to display the new data
-                    frm.refresh_field('vehicle');
+						const logName = selectedItem; 
+						// Fetch the driver data for the selected Daily Log
+						frappe.call({
+							method: 'frappe.client.get',
+							args: {
+								doctype: 'Daily Log',
+								name: logName,
+								filters: ['driver'] 
+							},
+							callback: function(response) {
+								if (response.message) {
+									console.log(response.message)
+									const driverData = response.message;
+									const daily_log = driverData.name
+									const driver = driverData.driver;
+									const vehicle = driverData.vehicle_number
+									const newRow = frm.add_child('vehicle');
+									newRow.daily_log = daily_log 
+									newRow.driver = driver;
+									newRow.vehicle_name = vehicle;
+								
+								frm.refresh_field('vehicle');
+								
+								
+								} else {
+									console.error('Error fetching Daily Log data.');
+								}
+							}
+						});
+					});
+					
 				}
-			});
-			
-			
-		
+			});	
 			
         });
+		
     }
+	
 });
+
