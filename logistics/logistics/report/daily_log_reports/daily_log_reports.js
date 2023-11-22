2a7961acd7c1eb43468a81bee446c5089599a726
sivajyothis7 <sivajyothis9446@gmail.com>
2023-11-22 18:11:56 +0530

    fixes

---
+++ logistics/logistics/report/daily_log_reports/daily_log_reports.js
@@ -0,0 +1,48 @@
+frappe.query_reports["Daily Log Reports"] = {
+    "filters": [
+        
+        {
+            "fieldname": "customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer",
+        },
+        {
+            "fieldname": "driver",
+            "label": __("Driver"),
+            "fieldtype": "Link",
+            "options": "Drivers",
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+        },
+       
+        
+        {
+            "fieldname": "from",
+            "label": __("From Location"),
+            "fieldtype": "Data", 
+        },
+        {
+            "fieldname": "to",
+            "label": __("To Location"),
+            "fieldtype": "Data", 
+        },
+		{
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("company")
+        },
+        
+    
+    ],
+};
