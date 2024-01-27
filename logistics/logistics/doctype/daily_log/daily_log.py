d8fd8b09aec515912d2b1df57b4c8ed7899d7f51
sivajyothis7 <sivajyothis9446@gmail.com>
2024-01-27 12:21:04 +0530

    kkpp

--- logistics/logistics/doctype/daily_log/daily_log.py
+++ logistics/logistics/doctype/daily_log/daily_log.py
@@ -2,10 +2,10 @@ import frappe
 from frappe.model.document import Document
 
 class DailyLog(Document):
-    
+
     def on_update(self):
-        driver_doc = frappe.get_doc("Drivers", self.driver)
-        
+        driver_doc = frappe.get_doc("Drivers", self.pay_to)
+
         existing_payment_data = next((data for data in driver_doc.payment_data if data.daily_log == self.name), None)
 
         if existing_payment_data:
@@ -30,5 +30,38 @@ class DailyLog(Document):
         driver_doc.save(ignore_permissions=True)
         frappe.db.commit()
 
-        driver_link = frappe.utils.get_link_to_form(driver_doc.doctype, self.driver)
+        driver_link = frappe.utils.get_link_to_form(driver_doc.doctype, self.pay_to)
         frappe.msgprint("Driver Payment Data updated successfully for Driver: {}".format(driver_link))
+
+        # self.generate_or_update_way_bill()
+
+    def generate_way_bill(self):
+        if self.way_bill_collected:
+            if not self.waybill_number:
+                way_bill = frappe.get_doc({
+                    "doctype": "Way Bill",
+                    "daily_log": self.name,
+                    "date": frappe.utils.today(),
+                })
+
+                way_bill.insert(ignore_permissions=True)
+                frappe.msgprint(f"Way Bill generated successfully: {way_bill.name}")
+
+                self.waybill_number = way_bill.name
+                self.save(ignore_permissions=True)
+            else:
+                way_bill = frappe.get_doc("Way Bill", self.waybill_number)
+                way_bill.update({
+                    "daily_log": self.name,
+                    "date": frappe.utils.today(),
+                })
+
+                way_bill.save(ignore_permissions=True)
+                frappe.msgprint(f"Way Bill updated successfully: {way_bill.name}")
+        else:
+            frappe.msgprint("Way Bill generation is only allowed if Way bill collected is Checked.")
+
+@frappe.whitelist()
+def generate_wayy_bill(docname):
+    doc = frappe.get_doc("Daily Log", docname)
+    doc.generate_way_bill()
