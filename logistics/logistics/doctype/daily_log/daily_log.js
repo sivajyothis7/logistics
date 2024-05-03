297a66eef58ebad96b4745d325e6fdfaf5dab958
sivajyothis7 <sivajyothis9446@gmail.com>
2024-05-03 09:13:04 +0530

    kkp

--- logistics/logistics/doctype/daily_log/daily_log.js
+++ logistics/logistics/doctype/daily_log/daily_log.js
@@ -1,5 +1,8 @@
 frappe.ui.form.on('Daily Log', {
     refresh: function(frm) {
+        add_custom_button(frm);
+
+        
     },
 
     driver_rate: function(frm) {
@@ -25,7 +28,7 @@ frappe.ui.form.on('Daily Log', {
             frappe.validated = false;
             return;
         }
-    }
+    },
 });
 
 function calculateDriverPendingRate(frm) {
@@ -37,4 +40,23 @@ function calculateDriverPendingRate(frm) {
     frm.set_value('pending_driver_rate', driverPendingRate);
 }
 
-// 
+
+function add_custom_button(frm) {
+    
+    frm.add_custom_button(__('Generate Waybill'), function() {
+        frappe.call({
+            method: 'logistics.logistics.doctype.daily_log.daily_log.generate_waybilll',
+            args: {
+                docname: frm.docname
+            },
+            callback: function(response) {
+                if (response.message) {
+                    frm.reload_doc();
+                    // frappe.msgprint('Waybill generated successfully.');
+                } else {
+                    frappe.msgprint('Failed to generate waybill.Please Make sure Invoice Status is "Created"');
+                }
+            }
+        });
+    });
+}
