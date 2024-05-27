588b34d9fa63952439ab73428bfbcb99e36e6a8a
sivajyothis7 <sivajyothis9446@gmail.com>
2024-05-27 12:34:31 +0530

    new

--- logistics/logistics/doctype/daily_log/daily_log.py
+++ logistics/logistics/doctype/daily_log/daily_log.py
@@ -2,15 +2,15 @@ import frappe
 from frappe.model.document import Document
 
 class DailyLog(Document):
-    
     def on_update(self):
-        # Get the driver document associated with this daily log
+        self.update_driver_payment_data()
+        self.generate_waybill_if_needed()
+
+    def update_driver_payment_data(self):
         driver_doc = frappe.get_doc("Drivers", self.driver)
         
-        # Find if there is already payment data for this daily log in the driver document
         existing_payment_data = next((data for data in driver_doc.payment_data if data.daily_log == self.name), None)
 
-        # Update existing payment data or create new payment data
         if existing_payment_data:
             existing_payment_data.update({
                 "company_rate": self.company_rate,
@@ -30,34 +30,20 @@ class DailyLog(Document):
             }
             driver_doc.append("payment_data", payment_data)
 
-        # Save changes to the driver document
         driver_doc.save(ignore_permissions=True)
         frappe.db.commit()
 
-        # Provide feedback to the user
         driver_link = frappe.utils.get_link_to_form(driver_doc.doctype, self.driver)
         frappe.msgprint(f"Driver Payment Data updated successfully for Driver: {driver_link}")
 
-    
-@frappe.whitelist()
-def generate_waybill(daily_log):
-    if not daily_log:
-        frappe.msgprint("Daily Log document is required to generate the waybill.")
-        return
+    def generate_waybill_if_needed(self):
+        if not self.waybill_number:
+            waybill = frappe.get_doc({
+                "doctype": "Way Bill",
+                "daily_log": self.name,
+            })
+            waybill.insert(ignore_permissions=True)
+            frappe.msgprint(f"Way Bill generated successfully: {waybill.name}")
 
-    doc = frappe.get_doc("Daily Log", daily_log)
-
-    if doc.waybill_number:
-        frappe.throw(f"Way Bill already exists for this Daily Log: {doc.waybill_number}")
-    else:
-        data = frappe.get_doc({
-            "doctype": "Way Bill",
-            "daily_log": doc.name,
-        })
-        data.insert(ignore_permissions=True)
-        frappe.msgprint(f"Way Bill generated successfully: {data.name}")
-
-        doc.waybill_number = data.name
-        doc.save(ignore_permissions=True)
-
-        return doc.waybill_number
+            self.waybill_number = waybill.name
+            self.save(ignore_permissions=True)
